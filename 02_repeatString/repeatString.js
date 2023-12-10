cf6e51a03efb8649fb8eddd092bf49526882f2ea
timotius22 <37534035+timotius22@users.noreply.github.com>
2023-12-10 12:29:29 +0100

    solve excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(text, times) {
+    if (times < 0){
+        return "ERROR"
+    } else {
+        let newText = "";
+        for(let i=0; i < times; i++){
+        newText = newText + text;
+        }
+        return newText
+    }
+}
 
-};
+console.log(repeatString("hey", 5))
 
 // Do not edit below this line
 module.exports = repeatString;
