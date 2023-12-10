369236d9c4d178097023fd3845920aa0cc8269b0
timotius22 <37534035+timotius22@users.noreply.github.com>
2023-12-10 12:29:40 +0100

    solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let lettersInArray = text.split("");
+    console.log(lettersInArray);
+    let reversedString = "";
+    for (let i = lettersInArray.length - 1; i >= 0; i--){
+        reversedString += lettersInArray[i];
+    }  
+    return reversedString; 
 };
+
+console.log(reverseString("hello world"));  
 
 // Do not edit below this line
 module.exports = reverseString;
