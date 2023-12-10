dc746978072fe5746af7b04092af5b9a94da949c
timotius22 <37534035+timotius22@users.noreply.github.com>
2023-12-10 12:29:49 +0100

    solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...args) {
+    let filteredArray = inputArray;
+    for (let i = 0; i < args.length; i++){
+        filteredArray = filteredArray.filter(argument => argument !== args[i])
+        console.log(typeof inputArray)
+        console.log(typeof args[i])
+        console.log(filteredArray)
+    }    
+    return filteredArray;
 };
+
+console.log(removeFromArray([1,2,3,4,5], 1, "3",5)) 
 
 // Do not edit below this line
 module.exports = removeFromArray;
